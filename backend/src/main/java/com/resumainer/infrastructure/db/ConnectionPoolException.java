43ee79b22d2d8ddc1d5d2795bbf3741178d2b305
anton-ch <31995314+Anton-Ch@users.noreply.github.com>
2026-06-04 21:51:38 +0500

    feat(004-jdbc-pool): add ConnectionPoolException

---
+++ backend/src/main/java/com/resumainer/infrastructure/db/ConnectionPoolException.java
@@ -0,0 +1,12 @@
+package com.resumainer.infrastructure.db;
+
+public class ConnectionPoolException extends RuntimeException {
+
+    public ConnectionPoolException(String message) {
+        super(message);
+    }
+
+    public ConnectionPoolException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
